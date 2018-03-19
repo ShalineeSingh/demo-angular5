f62066458f33e97d96c76eb9ab6d70f35bdbadeb
shalineesingh <shalinee.singh@coviam.com>
2018-03-19 17:34:54 +0530

    added two more components

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  styleUrls: ['./server.component.css'],
+  templateUrl: './server.component.html'
+})
+
+export class ServerComponent { }
